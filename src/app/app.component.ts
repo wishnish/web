a4c1bf364238bfebeda9386836e44db5b33e3cc9
wishnish <nihalnishwith77@gmail.com>
2024-02-16 10:58:08 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MyinfoComponent } from './pages/myinfo/myinfo.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,MyinfoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/pages/myinfo/myinfo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-myinfo',
+  standalone: true,
+  imports: [],
+  templateUrl: './myinfo.component.html',
+  styleUrl: './myinfo.component.css'
+})
+export class MyinfoComponent {
+
+}
